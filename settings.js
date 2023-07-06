2257fea18c092a5f0b65247b321035b7e4eb0f95
Pukuda <107295225+Pukuda@users.noreply.github.com>
2023-07-06 07:27:53 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Fayy'// yourname
+global.MONGO_DB_URI = "mongodb+srv://Cluster0:avwGQCSUgB3OWak7@cluster0.9l4dei7.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "popobudak@gmail.com" //email
+global.email_password = "mqnuvqrndauvpsdd" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
